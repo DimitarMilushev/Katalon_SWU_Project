074eb9c7e05a06daad3a0b672059bbdb57a300e0
D_M <deadlycookye@gmail.com>
2022-09-01 13:23:51 +0300

    init project

---
+++ Test Suites/LoginTestSuite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>LoginTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>8330da32-58d1-40e7-adc0-a03f40dd3979</testSuiteGuid>
+   <testCaseLink>
+      <guid>3d8b1864-692a-4eb3-b9c4-db5ea8ca579e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/LoginTests/UserIsRedirectedToHomeAfterLogin</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
